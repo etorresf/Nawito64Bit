4456ad40a5a59e74fcf9a232704cd463ecd53afe
Ernesto Torres Feliciano <52752957+etorresf@users.noreply.github.com>
2021-01-05 16:22:33 -0500

    Add files via upload

---
+++ protocol.h
@@ -0,0 +1,33 @@
+/*
+ * < Nawito is a chess engine winboard derived of Danasah507>
+ * Copyright (C) <2020> <Ernesto Torres Feliciano>
+ *
+ * E-mail:		<ernesto2@nauta.cu>
+ * Source:		https:*github.com/etorresf/Nawito
+ * Web page:	*****************************
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include "book.h"
+
+
+void				Xboard(void);								// Protocolo xboard mas otros commandos
+
+
+#endif // PROTOCOL_H
